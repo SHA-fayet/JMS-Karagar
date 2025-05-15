519ea3902727d10366fa3cc5e9b1960479bed843
SHA-fayet <107316920+SHA-fayet@users.noreply.github.com>
2025-05-15 16:50:42 +0600

    search.py

--- search.py
+++ search.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request
 from app import mysql
 from .auth import login_required
+import MySQLdb.cursors  
 
 search_bp = Blueprint('search', __name__)
 
@@ -10,22 +11,22 @@ def search():
     query = request.args.get('q', '')
     category = request.args.get('category', 'inmates')
     results = []
-    
+
     if query:
-        cur = mysql.connection.cursor()
-        
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)  
+
         if category == 'inmates':
-            cur.execute("SELECT * FROM inmates WHERE name LIKE %s", (f'%{query}%',))
+            cur.execute("SELECT id, name, crime, sentence, status FROM inmates WHERE name LIKE %s", (f'%{query}%',))
             results = cur.fetchall()
-            
+
         elif category == 'visitors':
-            cur.execute("SELECT * FROM visitors WHERE visitor_name LIKE %s", (f'%{query}%',))
+            cur.execute("SELECT id, visitor_name, relation, visit_date, inmate_id FROM visitors WHERE visitor_name LIKE %s", (f'%{query}%',))
             results = cur.fetchall()
-            
+
         elif category == 'cells':
-            cur.execute("SELECT * FROM cells WHERE cell_number LIKE %s", (f'%{query}%',))
+            cur.execute("SELECT id, cell_number, current_occupancy FROM cells WHERE cell_number LIKE %s", (f'%{query}%',))
             results = cur.fetchall()
-            
+
         cur.close()
-    
+
     return render_template('search.html', results=results, query=query, category=category)
